8af79a3e48f29d03d8318e2e4f01a9a46850868a
Joshua Emele <jemele@acm.org>
2015-12-08 13:05:46 -0800

    Updated xillybus_wrapper signature, ifd out cordic

--- Demo/wbfm.cpp
+++ Demo/wbfm.cpp
@@ -30,7 +30,7 @@ void fir(float * result, float input, unsigned short do_filter)
 /* **** */
 
 /* VOLK */
-void volk(float outputVector[MYCOUNT], float inputVector[MYCOUNT])
+inline void volk(float outputVector[MYCOUNT], float inputVector[MYCOUNT])
 {
 #pragma HLS pipeline II=1024
 	for(int i = 0; i <MYCOUNT-4; i+=2){
@@ -53,7 +53,7 @@ static float b1 = 0.964424;
 static float input_minus_one = 0.0;
 static float output_minus_one = 0.0;
 
-void iir(float input, float *output, int size)
+inline void iir(float input, float *output, int size)
 {
 	*output = input*b0+input_minus_one*b1 + output_minus_one*a1;
 	output_minus_one = *output;
@@ -61,6 +61,88 @@ void iir(float input, float *output, int size)
 }
 /* **** */
 
+#if 0
+#include <ap_fixed.h>
+typedef ap_fixed<64,3> fix_num;
+const int NO_ITER = 16;
+const fix_num pi2 = 1.57079632679;
+fix_num angles[NO_ITER] = {0.785398163397448,	0.463647609000806,
+    0.244978663126864,	0.124354994546761,	0.0624188099959574,
+    0.0312398334302683,	0.0156237286204768,	0.00781234106010111,
+    0.00390623013196697,	0.00195312251647882,	0.000976562189559320,
+    0.000488281211194898,	0.000244140620149362,	0.000122070311893670,
+    6.10351561742088e-05,	3.05175781155261e-05};
+
+void correct_quad(float * x, float * y, int * quad)
+  {
+
+  	float temp = 0;
+
+  	if (*x < 0) {
+  		if (*y<0) {
+  			temp = -*y; // temp = -1*y
+  			* y = *x;
+  			* x = temp;
+  			*quad = 3;
+  		} else {
+  			temp = -*x;  // temp  = -1*x
+  			* x = *y;
+  			* y = temp;
+  			*quad = 2;
+  		}
+  	}
+  }
+
+  void restore_quad(fix_num *theta, int quad){
+
+  	if (quad == 2) {
+  		*theta += (pi2);
+  	}
+  	if (quad == 3){
+  		*theta -= (pi2);
+  	}
+  }
+
+
+  float fast_atan(float x, float y)
+  {
+	// write your code here
+	/* normalize to +/- 45 degree range */
+	const float y_abs = fabsf(y);
+	const float x_abs = fabsf(x);
+	/* don't divide by zero! */
+	if(!((y_abs > 0.0f) || (x_abs > 0.0f))) {
+		return 0.0;
+    }
+
+	  	fix_num theta = 0;
+
+	  	int quad = 0;
+	  	correct_quad(&x, &y, &quad);
+	  	fix_num temp = 0;
+
+	  	fix_num xx = fix_num(x);
+	  	fix_num yy = fix_num(y);
+
+		for (int i = 0; i < NO_ITER; i++){
+	  		if (yy <= 0) {
+	  			temp=xx;
+	  			xx -= yy>>i;
+	  			yy += temp>>i;
+	  			theta = theta - angles[i];
+	  		} else {
+	  			temp=xx;
+	  			xx += yy>>i;
+	  			yy -= temp>>i;
+	  			theta = theta + angles[i];
+
+	  		}
+	  	}
+	  	restore_quad(&theta, quad);
+	  	return(float(theta));
+  }
+
+#else 
   #define TAN_MAP_RES 0.003921549 /* (smallest non-zero value in table) */
 
   #define RAD_PER_DEG 0.017453293
@@ -139,38 +221,22 @@ void iir(float input, float *output, int size)
   };
 
   /*****************************************************************************
-
    Function: Arc tangent
-
-
    Syntax: angle = fast_atan2(y, x);
-
    float y y component of input vector
-
    float x x component of input vector
-
    float angle angle of vector (x, y) in radians
 
-
-   Description: This function calculates the angle of the vector (x,y)
-
-   based on a table lookup and linear interpolation. The table uses a
-
-   256 point table covering -45 to +45 degrees and uses symetry to
-
-   determine the final angle value in the range of -180 to 180
-
-   degrees. Note that this function uses the small angle approximation
-
-   for values close to zero. This routine calculates the arc tangent
-
-   with an average error of +/- 3.56e-5 degrees (6.21e-7 radians).
-
+   Description: This function calculates the angle of the vector (x,y) based on
+   a table lookup and linear interpolation. The table uses a 256 point table
+   covering -45 to +45 degrees and uses symetry to determine the final angle
+   value in the range of -180 to 180 degrees. Note that this function uses the
+   small angle approximation for values close to zero. This routine calculates
+   the arc tangent with an average error of +/- 3.56e-5 degrees (6.21e-7
+   radians).
   *****************************************************************************/
-  float fast_atan(float y, float x)
+  inline float fast_atan(float y, float x)
   {
-#pragma HLS pipeline
-
 	float x_abs, y_abs, z;
 	float alpha, angle, base_angle;
 	int index;
@@ -236,44 +302,40 @@ void iir(float input, float *output, int size)
 
 	return (angle);
   }
+#endif
 
 /* XILLYBUS WRAPPER */
 static float d_gain = 0.002122;
 
-void xillybus_wrapper(int *in, int *out)
+void xillybus_wrapper(float *in, float *out)
 {
 #pragma AP interface ap_fifo port=in
 #pragma AP interface ap_fifo port=out
 #pragma AP interface ap_ctrl_none port=return
+#pragma HLS dataflow
 
-	int x1, y1;
-	int i = 0, j = 0;
 	float inf[MYCOUNT];
 #pragma HLS ARRAY_PARTITION variable=inf complete dim=1
 
 	float outf[MYCOUNT];
 #pragma HLS ARRAY_PARTITION variable=outf complete dim=1
 
-	float y11;
-	/* arc tan inputs */
-	float c, d, result, tempb;
-
-	for( i = 0; i < MYCOUNT; i++) {
-		x1 = *in++;
-		inf[i] = *((float *)&x1);
+	for (int i = 0; i < MYCOUNT; ++i) {
+#pragma HLS pipeline
+		inf[i] = *in++;
 	}
 
+	// generate the magnitude using conjugate multiply
 	volk(outf, inf);
 
-	for( i = 0, j = 0; i < MYCOUNT-1; i=i+2, j++) {
-		c = fast_atan(outf[i+1], outf[i]); //image, real
-		d = d_gain * c;
+	for (int i = 0, j = 0; i < MYCOUNT-1; i=i+2, ++j) {
+#pragma HLS pipeline
+		const float c = fast_atan(outf[i+1], outf[i]); //image, real
+		const float d = d_gain * c;
+		float tempb;
 		fir(&tempb, d, j%4);
 		if (j%4 == 0) {
-		  iir(tempb, &result, 1);
-		  y11 = result;
-		  y1 = *((int *) &y11);
-		  *out++ = y1;
+		  iir(tempb, out++, 1);
 		}
 	}
 }
