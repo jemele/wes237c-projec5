3bcfdb78c0f74c92609d1f5a86a42834e42004ca
Joshua Emele <jemele@acm.org>
2015-12-09 21:07:02 -0800

    Land testbench
    This fails after the 40th term...
    
    @I [HLS-10] Setting target device to 'xc7z020clg484-1'
    make: `csim.exe' is up to date.
    39, output=-0.0004636970        golden=-0.0005212677
    40, output=-0.0011943741        golden=-0.0015457752
    41, output=-0.0007688857        golden=-0.0017457556
    42, output=-0.0011622229        golden=-0.0018855283
    43, output=-0.0018895230        golden=-0.0026417025
    44, output=-0.0013078322        golden=-0.0025352996
    45, output=-0.0006842724        golden=-0.0013132739
    46, output=-0.0006841076        golden=-0.0009190433
    ...

--- Demo/script.tcl
+++ Demo/script.tcl
@@ -6,8 +6,12 @@
 open_project HLS
 set_top xillybus_wrapper
 add_files wbfm.cpp
+add_files -tb myinput.txt
+add_files -tb golden.txt
+add_files -tb WBFM_test.cpp
 open_solution "solution1"
 set_part {xc7z020clg484-1}
 create_clock -period 10 -name default
 source "./directives.tcl"
-csynth_design
+csim_design
+#csynth_design
--- Demo/wbfm.h
+++ Demo/wbfm.h
@@ -5,11 +5,12 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define MYCOUNT  2048
+#define MYCOUNT  4096 // it is MYCOUNT/2 complex numbers
 
 void volk(float outputVector[MYCOUNT], float inputVector[MYCOUNT]);
 void fir(float * result, float input, unsigned short do_filter);
 void iir(float input, float *output, int size);
 float fast_atan(float y, float x);
+void xillybus_wrapper(float *in, float *out);
 
 #endif
