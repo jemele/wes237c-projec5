9fd224e492bab36123940deda2141d20c3732340
Joshua Emele <jemele@acm.org>
2015-12-10 14:50:29 -0800

    ip, initial add
    With MYCOUNT 128 we bust timing, so we're trying for 64

---
+++ ip/script.tcl
@@ -0,0 +1,15 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 2012 Xilinx Inc. All rights reserved.
+############################################################
+open_project hls
+set_top wbfm
+add_files wbfm.c
+add_files xilly_debug.c
+open_solution "solution1"
+set_part  {xc7z020clg484-1}
+create_clock -period 10
+config_interface -all ap_fifo -expose_global
+source "directives.tcl"
+csynth_design
---
+++ ip/wbfm.h
@@ -0,0 +1,16 @@
+#ifndef WBFM_H
+#define WBFM_H
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define MYCOUNT  64 // it is MYCOUNT/2 complex numbers
+
+void volk(float outputVector[MYCOUNT], float inputVector[MYCOUNT]);
+void fir(float * result, float input, unsigned short do_filter);
+void iir(float input, float *output, int size);
+float fast_atan(float y, float x);
+void wbfm(float *in, float *out);
+
+#endif
